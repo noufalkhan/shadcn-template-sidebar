809961b3adcc2ae3af2facc5084dba2fad7a3dc8
noufalkhan <126069577+noufalkhan@users.noreply.github.com>
2026-04-29 11:26:55 +0530

    Add files via upload

---
+++ app/page.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import DashboardPage from './dashboard/page'
+
+const page = () => {
+  return (
+    <div>
+     
+      <DashboardPage />
+    </div>
+  )
+}
+
+export default page
